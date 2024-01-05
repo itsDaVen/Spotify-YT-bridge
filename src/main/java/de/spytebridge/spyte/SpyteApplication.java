8f05f6fa033536ab97b07e1f0f798ca666f0c04a
Manuel Trapp <manuel.trapp@msg.group>
2024-01-06 00:50:29 +0100

    init

---
+++ src/main/java/de/spytebridge/spyte/SpyteApplication.java
@@ -0,0 +1,13 @@
+package de.spytebridge.spyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/spytebridge/spyte/SpyteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.spytebridge.spyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
